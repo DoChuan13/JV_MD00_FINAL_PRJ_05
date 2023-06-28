0edcccd5a0006bc379c96046a36d7d48a1c624c8
DoChuan13 <chuando.9213@gmail.com>
2023-06-28 16:13:41 +0700

    Add Logger

--- src/main/java/backend/socket/WebSocketController.java
+++ src/main/java/backend/socket/WebSocketController.java
@@ -2,6 +2,8 @@ package backend.socket;
 
 import backend.socket.dto.MessageDTO;
 import backend.socket.dto.WebsocketDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -11,10 +13,13 @@ import org.springframework.web.util.HtmlUtils;
 @Controller
 @CrossOrigin(origins = {"*"})
 public class WebSocketController {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
+
     @MessageMapping("/hello")
     @SendTo("/topic/chat")
     public WebsocketDTO greeting(MessageDTO message) throws Exception {
         Thread.sleep(1000); // simulated delay
+        logger.info("Response===>" + message.getChat().getId());
         /*return new WebsocketDTO(HtmlUtils.htmlEscape(String.valueOf(message)));*/
         return new WebsocketDTO(HtmlUtils.htmlEscape(String.valueOf(message.getChat().getId())));
     }
--- src/main/java/backend/socket/WebsocketConfig.java
+++ src/main/java/backend/socket/WebsocketConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-@CrossOrigin(origins = {"*"})
+//@CrossOrigin(origins = {"*"})
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
