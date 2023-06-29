20ef9e5d49560752c0e2efe0002fdae10e258b66
DoChuan13 <chuando.9213@gmail.com>
2023-06-29 21:36:15 +0700

    Add CORS Config

--- src/main/java/backend/socket/WebsocketConfig.java
+++ src/main/java/backend/socket/WebsocketConfig.java
@@ -1,8 +1,11 @@
 package backend.socket;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,6 +14,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 @CrossOrigin(origins = {"*"})
 public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final String url = "http://localhost:4200";
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
@@ -20,8 +25,22 @@ public class WebsocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins(url).withSockJS();
         registry.addEndpoint("/ws").setAllowedOrigins("https://fakebook-dochuan.netlify.app").withSockJS();
         registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:4200").withSockJS();
         /*registry.addEndpoint("/ws").withSockJS();*/
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/ws/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowedOrigins(url);
+            }
+        };
     }
 }
